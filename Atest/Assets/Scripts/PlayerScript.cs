5f74f9b5d233e82caf9b86063a828aebe2680785
picalexs <picalexs@gmail.com>
2023-01-23 22:03:40 +0200

    Player Movement
    Move+ Basic Jump

---
+++ Atest/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField, Tooltip("Max speed, in units per second, that the character moves.")]
+    float speed = 9;
+
+    [SerializeField, Tooltip("Acceleration while grounded.")]
+    float walkAcceleration = 75;
+
+    [SerializeField, Tooltip("Acceleration while in the air.")]
+    float airAcceleration = 30;
+
+    [SerializeField, Tooltip("Deceleration applied when character is grounded and not attempting to move.")]
+    float groundDeceleration = 70;
+
+    [SerializeField, Tooltip("Max height the character will jump regardless of gravity")]
+    float jumpHeight = 4;
+
+    private BoxCollider2D boxCollider;
+
+    private Vector2 velocity;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(2 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+        }
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        if (moveInput != 0)
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime);
+        }
+        else
+        {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
+        }
+        transform.Translate(velocity * Time.deltaTime);
+
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit == boxCollider)
+                continue;
+            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
+
+            if (colliderDistance.isOverlapped)
+            {
+                transform.Translate(colliderDistance.pointA - colliderDistance.pointB);   
+            }
+        }
+    }
+}
