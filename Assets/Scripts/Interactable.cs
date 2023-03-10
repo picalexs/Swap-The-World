a2cb22183dca516d0e43131fca0d8a3a6ae8d36e
picalexs <picalexs@gmail.com>
2023-03-10 14:43:46 +0200

    Merge Bug Fix

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -6,21 +6,17 @@ using UnityEngine.Events;
 public class Interactable : MonoBehaviour
 {
     public bool isInRange;
-    [SerializeField] int sceneNumber;
     public KeyCode interactKey;
     public UnityEvent interactAction;
-    [SerializeField] bool isUnlocked;
-    public LevelUnlockingManager level;
     void Start()
     {
-       isUnlocked=LevelUnlockingManager.unlocked[sceneNumber];
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         isUnlocked=LevelUnlockingManager.unlocked[sceneNumber];
-        if(isInRange && isUnlocked)
+        if(isInRange)
         {
             if(Input.GetKeyDown(interactKey))
             {
