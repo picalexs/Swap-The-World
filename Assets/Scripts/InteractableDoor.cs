237626d1d34978aa579da03536b788910dde8ac5
Mantelgen <84387756+Mantelgen@users.noreply.github.com>
2023-04-02 17:26:22 +0300

    yes

--- Assets/Scripts/InteractableDoor.cs
+++ Assets/Scripts/InteractableDoor.cs
@@ -8,17 +8,53 @@ public class InteractableDoor : MonoBehaviour
     public bool isInRange;
     public KeyCode interactKey;
     public UnityEvent interactAction;
-    
+    [SerializeField] private int levelNumber;
+    [SerializeField] public int doors;
+    [SerializeField] private GameObject player;
+    [SerializeField] public bool LevelDoor;
+    [SerializeField] public bool ExitDoor;
+    [SerializeField] private bool previousLevelComplet;
+    private void Start() {
+        doors= player.GetComponent<PlayerDataSave>().doorsUnlocked;
+        //player.GetComponent<PlayerDataSave>().LoadData();
+        //player.GetComponent<PlayerDataSave>().ResetData();
+    }
     void Update()
     {
+        LevelDoor=player.GetComponent<PlayerDataSave>().isLevelDoor;
+        ExitDoor=player.GetComponent<PlayerDataSave>().isExitDoor;
+        previousLevelComplet= (doors>=levelNumber);
         if(isInRange)
         {
-            if(Input.GetKeyDown(interactKey))
+            if(LevelDoor)
             {
-                interactAction.Invoke();
+                if(previousLevelComplet)
+                {
+                    if(Input.GetKeyDown(interactKey))
+                        {
+                           
+                                interactAction.Invoke();
+                        } 
+                }                
+                else
+                {
+                    Debug.Log("nu poti intra");
+                }
             }
+            else if(ExitDoor)
+            {
+                if(Input.GetKeyDown(interactKey))
+                    {
+                    player.GetComponent<PlayerDataSave>().doorsUnlocked=Mathf.Max(player.GetComponent<PlayerDataSave>().doorsUnlocked,levelNumber+1);
+                    Debug.Log(player.GetComponent<PlayerDataSave>().doorsUnlocked);
+                    player.GetComponent<PlayerDataSave>().SaveData();
+                    Debug.Log("am crescut");
+                    interactAction.Invoke();
+                    }         
+            }
+            }
+            
         }
-    }
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.gameObject.CompareTag("Player"))
         {
